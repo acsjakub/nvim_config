8b69be535113f0efe3a201bd4e6090cf9e8abfb8
Jakub Acs <acsjakub@gmail.com>
2024-12-21 13:39:25 +0100

    lsp, keys: add keymap to search workspace
    Create a command and corresponding keymap to search workspace for a
    symbol. Currently there is no prompt or anything, just type 'SPC g s'
    and continue with your query.
    
    Signed-off-by: Jakub Acs <acsjakub@gmail.com>

--- lua/acsjakub/lsp.lua
+++ lua/acsjakub/lsp.lua
@@ -17,11 +17,27 @@ function lsp_keymap(info)
 	for key, action in pairs(keymaps) do
 		vim.api.nvim_buf_set_keymap(info.buf, "n", key, action, opts)
 	end
-	
+
+	vim.api.nvim_buf_set_keymap(
+		info.buf,
+		"n",
+		"<Leader>gs",
+		":AcsjakubLSPSearchWorkspace ",
+		opts
+	)
+
 end
 
 lspconfig.rust_analyzer.setup({})
 lspconfig.clangd.setup({})
+
+vim.api.nvim_create_user_command(
+	"AcsjakubLSPSearchWorkspace",
+	function(arg)
+		vim.lsp.buf.workspace_symbol(arg.args)
+	end ,
+	{ nargs=1 }
+)
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = lsp_keymap
