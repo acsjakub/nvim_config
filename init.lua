9a668589dfdd2a9c2748472ae706631d0a90d912
Jakub Acs <acsjakub@gmail.com>
2024-12-21 15:34:38 +0100

    init: set colorscheme to habamax
    Until I settle on a good colorscheme, I chose habamax as the "best" one
    from the pre-defined.
    
    Signed-off-by: Jakub Acs <acsjakub@gmail.com>

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require "acsjakub.options" -- includes PWD/lua/acsjakub/options.lua file
 require "acsjakub.lsp"
 require "acsjakub.keymap"
 require "acsjakub.custom"
+
+vim.api.nvim_command("colorscheme habamax")
