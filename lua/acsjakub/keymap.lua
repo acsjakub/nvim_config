b13cace836abfa485e61035055ffa7b251490ad1
Jakub Acs <acsjakub@gmail.com>
2025-02-16 19:00:28 +0100

    rg_search: update keymaps
    Update used keymaps for searching with rg. Keep the more raw version for
    fallback situations.
    
    Signed-off-by: Jakub Acs <acsjakub@gmail.com>

--- lua/acsjakub/keymap.lua
+++ lua/acsjakub/keymap.lua
@@ -35,8 +35,9 @@ vim.api.nvim_set_keymap("n", "<Leader>e", ":Lex 20<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Leader>p", "<C-o>", opts)
 vim.api.nvim_set_keymap("n", "<Leader>n", "<C-i>", opts)
 
--- run ripgrep in the "current directory" (wait for input silently)
-vim.api.nvim_set_keymap("n", "<Leader>rg", ":term rg ", opts)
+-- ripgrep (wait for input silently)
+vim.api.nvim_set_keymap("n", "<Leader>rg", ":AcsjakubRG ", opts) -- populates quickfix list
+vim.api.nvim_set_keymap("n", "<Leader>rrg", ":term rg ", opts) -- as in "raw rg"
 
 vim.api.nvim_set_keymap("t", "<esc><esc>", "<c-\\><c-n>", opts)
 vim.api.nvim_set_keymap("n", "<Leader>t", ":AcsjakubTerminal<CR>", opts)
